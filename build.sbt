669b88ebb27da70d65f669144db96439cf148faf
scalolli <basavarajkalloli@gmail.com>
2013-02-12 23:26:07 +0530

    Added initial directories and a first entry point

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Data Loader"
+
+version := "0.1"
+
+scalaVersion := "2.10.0"
---
+++ src/main/scala/Runner.scala
@@ -0,0 +1,7 @@
+object Runner {
+  
+  def main(args:Array[String]) {
+	println("Welcome to small data loader !!")
+  }
+
+}
